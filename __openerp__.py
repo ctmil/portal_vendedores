e4f3340bf1f7d6095f0d0f35422e7943589364c1
morrillo <gustavo.orrillo@moldeo.coop>
2016-06-06 14:52:07 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Portal - Vendedores',
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['base','sale','product'],
+    'data': [
+	#'account_view.xml',
+	#'taxes_report.xml',
+	#'partner_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
