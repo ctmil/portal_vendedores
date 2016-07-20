962c18fdb007eb1f84a733745dbbaa62d5e8707c
morrillo <gustavo.orrillo@moldeo.coop>
2016-07-20 17:20:05 -0300

    Added stock_routing

--- models.py
+++ models.py
@@ -138,3 +138,22 @@ class account_invoice(models.Model):
 			if len(invoices) > 1:
 			        raise ValidationError("Fields name and description must be different")
 
+
+
+class sale_order_line(osv.osv):
+        _inherit = 'sale.order.line'
+
+
+        """     
+        def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,uom=False, qty_uos=0,\
+                 uos=False, name='', partner_id=False,lang=False, update_tax=True, date_order=False,\
+                 packaging=False, fiscal_position=False, flag=False, context=None):
+                import pdb;pdb.set_trace()
+                return super(sale_order_line, self).product_id_change(cr,uid,ids,pricelist,product,qty,uom,qty_uos,uos,name,\
+                                partner_id,lang,update_tax,date_order,packaging,fiscal_position,flag,context)
+        """
+
+
+        def _check_routing(self, cr, uid, ids, product, warehouse_id, context=None):
+                return True
+
